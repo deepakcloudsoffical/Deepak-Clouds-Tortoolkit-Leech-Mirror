a520ac1c79c9e661d9ee5dc8445ababcfa3367ef
Deepak Clouds <76053688+deepakcloudsoffical@users.noreply.github.com>
2021-06-30 15:42:00 +0530

    Add files via upload

---
+++ tortoolkit/consts/ExecVarsSample.py
@@ -0,0 +1,77 @@
+import os
+try:
+    from .ExecVars import ExecVars
+except:
+    class ExecVars:
+        # TODO optimize for vps use fully - currently only heroku is focused
+        # Set true if its VPS [currently not fully working]
+        IS_VPS = False
+        API_HASH = os.environ.get("API_HASH")
+        API_ID = int(os.environ.get("API_ID"))
+        BOT_TOKEN = os.environ.get("BOT_TOKEN")
+        BASE_URL_OF_BOT = os.environ.get("BASE_URL")
+        # ALLOWED USERS [ids of user or supergroup] seperate by commas
+        ALD_USR = [871967943,1291911714,755983475,1282644258,-1001348584170]
+        
+        # Time to wait before edit message
+        EDIT_SLEEP_SECS = 5
+
+        # Telegram Upload Limit (in bytes)
+        TG_UP_LIMIT = 2000000000
+
+        # Should force evething uploaded into Document
+        FORCE_DOCUMENTS = False
+
+        # Chracter to use as a completed progress 
+        COMPLETED_STR = "▰"
+
+        # Chracter to use as a incomplete progress
+        REMAINING_STR = "▱"
+
+        # DB URI for access
+        DB_URI = os.environ.get("DATABASE_URL")
+        
+        # The base direcory to which the files will be upload if using RCLONE
+        RCLONE_BASE_DIR = "/"
+
+        # This value will be considered only if Rclone is True - this may be defied now ;)
+        # Cuz at least one needs to be Ture at a time either RCLONE or Leech.
+        LEECH_ENABLED = True
+
+        # Will be enabled once its set
+        # For vps change it to True if config loaded
+        RCLONE_ENABLED = False
+
+        # If the user fails to select whether to use rclone or telegram to upload this will be the deafult.
+        DEFAULT_TIMEOUT = "leech"
+
+        # For vps set path here or you can use runtime too
+        RCLONE_CONFIG = False
+        
+        # Name of the RCLONE drive from the config
+        DEF_RCLONE_DRIVE = ""
+
+        # Max size of the torrent allowed
+        MAX_YTPLAYLIST_SIZE = 20
+        
+        # Max size of the torrent allowed
+        MAX_TORRENT_SIZE = 10
+
+        # This is to stop someone from abusing the system by imposing the limit
+        # [<GBs of total torrent sapce>, <Number of youtube videos allowed to download>, <Number of youtube playlists allowed to download>]
+        USER_CAP_ENABLE = False
+        USER_CAP_LIMIT = [50,10,5]
+
+        # No need to worry about these
+        # CHANGE THESE AT YOUR RISK
+        LOCKED_USERS = False
+        RSTUFF = False
+        FORCE_DOCS_USER = False
+        FAST_UPLOAD = True
+        METAINFO_BOT = False
+        
+
+
+
+
+
