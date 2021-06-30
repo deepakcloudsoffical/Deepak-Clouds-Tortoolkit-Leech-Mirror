89c969032410fd586bb4f3b3834996990c272d7c
Deepak Clouds <76053688+deepakcloudsoffical@users.noreply.github.com>
2021-06-30 15:42:25 +0530

    Add files via upload

---
+++ tortoolkit/consts/DefaultCommands.py
@@ -0,0 +1,16 @@
+import os
+class Commands:
+    LEECH = os.environ.get("LEECH_CMD")
+    PURGE = os.environ.get("PURGE_CMD")
+    PAUSEALL = os.environ.get("PAUSEALL_CMD")
+    RESUMEALL = os.environ.get("RESUMEALL_CMD")
+    STATUS = os.environ.get("STATUS_CMD")
+    SETTINGS = os.environ.get("SETTINGS_CMD")
+    EXEC = os.environ.get("EXEC_CMD")
+    UPLOAD = os.environ.get("UPLOAD_CMD")
+    YTDL = os.environ.get("YTDL_CMD")
+    PYTDL = os.environ.get("PYTDL_CMD")
+    ABOUT = os.environ.get("ABOUT_CMD")
+    GETLOGS = os.environ.get("GETLOGS_CMD")
+    SERVER = os.environ.get("SERVER_CMD")
+    USERSETTINGS = os.environ.get("USERSETTINGS_CMD")
